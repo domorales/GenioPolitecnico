8ed4b175e13cde62f1ab73daf997a8b48be6ae87
domorales <domorale@espol.edu.ec>
2021-01-23 14:46:15 -0500

    se refactorizo

--- Aplicacion/src/main/java/com/espol/controller/VentanaJuegoController.java
+++ Aplicacion/src/main/java/com/espol/controller/VentanaJuegoController.java
@@ -16,7 +16,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
